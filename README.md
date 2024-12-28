0ad8fbcc5b825f24341abbe49a9cc059f308a230
BlankDev117 <mmmasent@gmail.com>
2024-12-28 14:09:01 -0500

    Throw HexagonalMetaData Exception and minor changes (OSK-5) (#6)
    Motivation
    ----
    Previously an invalid operation exception was being thrown when the
    HexagonalMetaDataException was expected
    
    Modifications
    ----
    * Throw expected exception
    * Updated UnitTests
    * Updated README and nuget docs for potential visibility
    
    Result
    ----
    Expected exception is thrown and some slightly updated docs
    
    Fixes #5

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # OSK.Hexagonal.MetaData
-This project is meant to provide a way to mark, directly in code, the expecation of a given abstraction for a hexagonally implemented
+This project is meant to provide a way to mark, directly in code, the expectation of a given abstraction for a hexagonally implemented
 library. By using this kind of metadata, a codebase may hopefully be more clearly documented to its intentions and usage. There is also
 a set of assembly extensions that can be used to retrieve specific types of abstractions based on their hexagonal integration type metadata.
 
 # Usage
 Codebase abstractions, i.e. interfaces, can be marked using the `HexagonalIntegrationAttribute` and can be marked in a variety of ways 
 to help provide code readers an idea to the intentions of the port at a glance. The attribute can be defined with one or more hexagonal integration
-types. 
+types. These types help to define expectations for integrations, consumers, and direct implementations of the library.
 
 Note: There is some additional logic in place to restrict some combinations of integration types. For example, an abstraction can 
 not be both marked as required and optional. This is to help reduce potential confusion for a codebase when using this library. The attribute
--- src/OSK.Hexagonal.MetaData/Internal/Helpers/HexagonalHelper.cs
+++ src/OSK.Hexagonal.MetaData/Internal/Helpers/HexagonalHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using OSK.Hexagonal.MetaData.Exceptions;
 
 namespace OSK.Hexagonal.MetaData.Internal.Helpers
 {
@@ -13,15 +14,15 @@ namespace OSK.Hexagonal.MetaData.Internal.Helpers
 
             if (isRequired && isOptional)
             {
-                throw new InvalidOperationException("Unable to set an integration type as both required and optional.");
+                throw new HexagonalMetaDataException("Unable to set an integration type as both required and optional.");
             }
             if (isRequired && hexagonalIntegrationTypes.Contains(HexagonalIntegrationType.LibraryProvided))
             {
-                throw new InvalidOperationException("Unable to set an integration type as required if it is already provided by the library.");
+                throw new HexagonalMetaDataException("Unable to set an integration type as required if it is already provided by the library.");
             }
             if (isOptional && hexagonalIntegrationTypes.Contains(HexagonalIntegrationType.ConsumerPointOfEntry))
             {
-                throw new InvalidOperationException("Unable to set an integration type as a consumer point of entry while it is optional");
+                throw new HexagonalMetaDataException("Unable to set an integration type as a consumer point of entry while it is optional");
             }
         }
     }
